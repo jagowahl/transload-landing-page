0ac01f1da3c419d66709833f1fadd638348bb702
Jago Wahl-Schwentker <jago@wahl-schwentker.de>
2025-01-12 13:07:53 +0100

    Fix color and remove dot in name

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer({ language }) {
 			<div className="container mx-auto px-4">
 				<div className="flex flex-col md:flex-row justify-between items-center">
 					<div className="mb-4 md:mb-0">
-						<p>&copy; 2025 transload. All rights reserved.</p>
+						<p>&copy; 2025 transload All rights reserved.</p>
 					</div>
 					<div className="flex space-x-4">
 						<Link
