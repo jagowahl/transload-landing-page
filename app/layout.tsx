f0671495102a600628310c5ddb8b3f98c4152e33
Nils Boerner <boerner_nils@gmx.de>
2025-01-21 19:39:38 +0100

    adding meta info and image

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,14 @@ import "@/app/_styles/globals.css";
 export const metadata = {
 	title: "transload",
 	description: "Revolutionizing Intralogistics",
+	keywords: ["Intralogistics", "Transshipment Warehouse", "Forklift", "Pallet Truck", "Route Optimization", "Cost Savings"],
+	openGraph: {
+		images: './logo_dark.png',
+	},
 	icons: {
 		icon: './favicon.png',
 	  },
+	
 };
 
 export default function RootLayout({
